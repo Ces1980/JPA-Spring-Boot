c3c8eb89231f5a0f70456e07b8eee0f47add7b24
César Vargas <thecesarvargas@gmail.com>
2020-01-15 17:59:31 -0600

    Creacion del archivo templates/listar(La vista en html)

--- src/main/java/com/ideasbolsa/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/ideasbolsa/springboot/app/controllers/ClienteController.java
@@ -12,20 +12,13 @@ import com.ideasbolsa.springboot.app.models.dao.IClienteDao;
 @Controller
 public class ClienteController {
 
-	/**
-	 * Se inicializa un objeto de la interfaz IClienteDao
-	 * para poder realizar las consultas
-	 *
-	 */
 	@Autowired
 	private IClienteDao clienteDao;
 	
-	/*Se usa requestMapping 
-	 * para marcar la ruta , pero de igual forma se puede usar @GetMapping*/
 	@RequestMapping(value="/listar", method =RequestMethod.GET)
 	public String listar(Model model) {
 		model.addAttribute("titulo", "Listado de clientes");
 		model.addAttribute("clientes", clienteDao.finAll());
-		return "ver";
+		return "listar";
 	}
 }
