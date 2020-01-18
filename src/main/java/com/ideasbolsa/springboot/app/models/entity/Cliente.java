c5673a134d5fe4d021a71b4cedb0611625c7ee94
César Vargas <thecesarvargas@gmail.com>
2020-01-17 19:42:45 -0600

    Agregando campo fecha en formulario y modificando el campo creatAt en clase Cliente.java

--- src/main/java/com/ideasbolsa/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/ideasbolsa/springboot/app/models/entity/Cliente.java
@@ -8,10 +8,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name = "clientes")
@@ -25,18 +26,16 @@ public class Cliente implements Serializable {
 	private String apellido;
 	private String email;
 
+	
+	//@DateTimeFormat(pattern = "yyyy-MM-dd") Especificar el formato de la fecha
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
 
 	private static final long serialVersionUID = 1L;
 
-	/*Método que permite generar la fecha de registro antes de enviar los datos
-	 * del formulario a la base de datos*/
-	@PrePersist
-	public void prePersist() {
-		createAt = new Date();
-	}
+	
 	public Long getId() {
 		return id;
 	}
