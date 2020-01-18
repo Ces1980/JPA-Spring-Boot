780a306f3a166eb4eb1bb7b6241387295d54c28d
César Vargas <thecesarvargas@gmail.com>
2020-01-18 15:57:25 -0600

    Implementado el Dao con la interfaz CrudRepository

--- src/main/java/com/ideasbolsa/springboot/app/models/dao/IClienteDao.java
+++ src/main/java/com/ideasbolsa/springboot/app/models/dao/IClienteDao.java
@@ -1,17 +1,11 @@
 package com.ideasbolsa.springboot.app.models.dao;
 
-import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
 
 import com.ideasbolsa.springboot.app.models.entity.Cliente;
 
-public interface IClienteDao {
-	//Método para listar
-	public List<Cliente> findAll();
-	//Método para guardar
-	public void save(Cliente cliente);
-	//Método para generar busqueda por id
-	public Cliente findOne(Long id);
-	//Método para elminar un registro
-	public void delete(Long id);
-
+public interface IClienteDao extends CrudRepository<Cliente, Long> {
+	
+	
 }
