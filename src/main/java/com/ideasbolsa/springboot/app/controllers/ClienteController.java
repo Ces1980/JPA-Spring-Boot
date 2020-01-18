2c050d03a76af8f34bbf0d661deae2efe94e900d
César Vargas <thecesarvargas@gmail.com>
2020-01-18 13:04:11 -0600

    Agregando la clase Service

--- src/main/java/com/ideasbolsa/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/ideasbolsa/springboot/app/controllers/ClienteController.java
@@ -15,23 +15,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
-import com.ideasbolsa.springboot.app.models.dao.IClienteDao;
+
 import com.ideasbolsa.springboot.app.models.entity.Cliente;
+import com.ideasbolsa.springboot.app.models.service.IClienteService;
 
 
 @Controller
 @SessionAttributes("cliente")
-/*@SessionAttributes("cliente") --> indicamos que se va a guardar en los atributos de la session el objeto
- * que se ha mapeado al formulario, que en este caso es cliente*/
+
 public class ClienteController {
 
 	@Autowired
-	private IClienteDao clienteDao;
+	private IClienteService clienteService;
 
 	@RequestMapping(value = "/listar", method = RequestMethod.GET)
 	public String listar(Model model) {
 		model.addAttribute("titulo", "Listado de clientes");
-		model.addAttribute("clientes", clienteDao.findAll());
+		model.addAttribute("clientes", clienteService.findAll());
 		return "listar";
 	}
 	
@@ -47,7 +47,7 @@ public class ClienteController {
 	public String editar(@PathVariable(value="id") Long id, Map<String, Object> model) {
 		Cliente cliente =null;
 		if (id > 0) {
-			cliente = clienteDao.findOne(id);
+			cliente = clienteService.findOne(id);
 		} else {
 			return "redirect:/listar";
 		}
@@ -56,17 +56,14 @@ public class ClienteController {
 		return"form";
 	}
 	
-	/*En el método guardar se agrerga un parametro (SessionStatus status) para
-	 *que se invoque el método setComplete() y así que el objeto cliente termine la session 
-	 *y se ejecute la accion de los métodos, ya sea guardar o eliminar, editar 
-	 * */
+	
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
 	public String guardar(@Valid @ModelAttribute("cliente") Cliente cliente, BindingResult result, Model model, SessionStatus status) {
 		if(result.hasErrors()) {
 			model.addAttribute("titulo", "Formulario de cliente");
 			return "form";
 		}
-		clienteDao.save(cliente);
+		clienteService.save(cliente);
 		status.setComplete();
 		return "redirect:listar";
 	}
@@ -76,7 +73,7 @@ public class ClienteController {
 	@RequestMapping(value="/eliminar/{id}")
 	public String eliminar(@PathVariable(value="id") Long id) {
 		if(id > 0) {
-			clienteDao.delete(id);
+			clienteService.delete(id);
 		}
 		return "redirect:/listar";
 	}
--- src/main/java/com/ideasbolsa/springboot/app/models/dao/ClienteDaoImpl.java
+++ src/main/java/com/ideasbolsa/springboot/app/models/dao/ClienteDaoImpl.java
@@ -6,7 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+
 
 import com.ideasbolsa.springboot.app.models.entity.Cliente;
 
@@ -18,7 +18,6 @@ public class ClienteDaoImpl implements IClienteDao {
 
 	/* Método crear */
 	@SuppressWarnings("unchecked")
-	@Transactional(readOnly = true)
 	@Override
 	public List<Cliente> findAll() {
 		return em.createQuery("from Cliente").getResultList();
@@ -26,14 +25,12 @@ public class ClienteDaoImpl implements IClienteDao {
 
 	/* Método buscar */
 	@Override
-	@Transactional(readOnly = true)
 	public Cliente findOne(Long id) {
 		return em.find(Cliente.class, id);
 	}
 
 	/* Método guardar/insertar o editar */
 	@Override
-	@Transactional
 	public void save(Cliente cliente) {
 		if (cliente.getId() != null && cliente.getId() > 0) {
 			/* Método que busca */
@@ -46,7 +43,6 @@ public class ClienteDaoImpl implements IClienteDao {
 
 	/* Método borrar */
 	@Override
-	@Transactional
 	public void delete(Long id) {
 		em.remove(findOne(id));
 	}
