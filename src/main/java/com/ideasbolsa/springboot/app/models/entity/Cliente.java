2cf7002404efe7c6772cd074d264671caf09b853
CesarVE <thecesarvargas@gmail.com>
2020-01-29 17:57:29 -0600

    Asociaciones @OneToMany Bidireccional --> Clases @Entity Factura y Cliente

--- src/main/java/com/ideasbolsa/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/ideasbolsa/springboot/app/models/entity/Cliente.java
@@ -1,13 +1,18 @@
 package com.ideasbolsa.springboot.app.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -25,34 +30,54 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	/* Configurando anotaciones de validación */
-	/*
-	 * @NotEmpty: que el campo no sea envíado sin caracteres--> sólo usado en los
-	 * String
-	 */
-	/* @Email: valida que el campo envie datos en formato de correo electronico */
-	/* @Size(min=4, max = 20): Limita el ingreso minimo y/o máximo de caracteres */
 	@NotEmpty
 	private String nombre;
+	
 	@NotEmpty
 	private String apellido;
+	
 	@NotEmpty
 	@Email
 	private String email;
 
-	/*
-	 * @NotNull: que el campo no sea envíado sin caracteres--> usando en campos con
-	 * caracteres numericos o caracteres
-	 */
 	@NotNull
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
 	@DateTimeFormat(pattern = "yyyy-mm-dd")
 	private Date createAt;
 
+	/*Anotación que indica que un cliente puede generar muchas facturas
+	 * One = > un cliente
+	 * ToMany = > muchas facturas*/
+	/* fetch = FetchType.LAZY = > Estrategia de carga de datos: peude ser EAGER o LAZY
+	* Se recomienda EAZY */
+	
+	/* cascade=CascadeType.ALL:
+	 * 
+	 * (Opcional) Las operaciones que deben conectarse en cascada 
+	 * con el objetivo de la asociación. 
+	 * El valor predeterminado es que no hay operaciones en cascada.
+	 * Cuando la colección de destino es un java.util.Map, 
+	 * el elemento en cascada se aplica al valor del mapa.
+	 * */
+	
+	/*mappedBy ="cliente"
+	 * 
+	 * Para relacionar ambas llaves en la base de datos
+	 * */
+	@OneToMany(mappedBy ="cliente" ,fetch = FetchType.LAZY, cascade=CascadeType.ALL)
+	private List<Factura> facturas;
+	
+	/**
+	 * Se crea un constructor para inicializar 
+	 * una factura cuando se crea un cliente*/
+	public Cliente() {
+		facturas = new ArrayList<Factura>();
+	}
+
 	private String foto;
 
-	private static final long serialVersionUID = 1L;
+	
 
 	public Long getId() {
 		return id;
@@ -94,8 +119,12 @@ public class Cliente implements Serializable {
 		this.createAt = createAt;
 	}
 
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
+
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
 	}
 
 	public String getFoto() {
@@ -105,5 +134,17 @@ public class Cliente implements Serializable {
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
+	
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	/**
+	 * Método que guarda la factura generada una por una*/
+	public void addFactura(Factura factura) {
+		facturas.add(factura);
+	}
+	
+	private static final long serialVersionUID = 1L;
 
 }
