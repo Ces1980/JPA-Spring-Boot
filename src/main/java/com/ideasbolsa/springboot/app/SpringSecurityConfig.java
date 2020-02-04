9042795a416eebfe4d65fca23cf3336872a87dc3
CesarVE <thecesarvargas@gmail.com>
2020-02-04 17:03:30 -0600

    Configuración de validaciones para darle seguridad a la aplicación

--- src/main/java/com/ideasbolsa/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/ideasbolsa/springboot/app/SpringSecurityConfig.java
@@ -1,50 +1,58 @@
 package com.ideasbolsa.springboot.app;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.ideasbolsa.springboot.app.auth.handler.LoginSuccesHandler;
+
+
+
 @Configuration
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private LoginSuccesHandler successHandler;
 	
-	
-	/*Método heredado de WebSecurityConfigurerAdapter --> clock derecho/source/Override/methods
-	 * y se despiegan todos los método que se pueden importar*/
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		/*Configurando los accesos según los roles*/
+
 		http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/listar").permitAll()
 		.antMatchers("/ver/**").hasAnyRole("USER")
 		.antMatchers("/uploads/**").hasAnyRole("USER")
 		.antMatchers("/form/**").hasAnyRole("ADMIN")
 		.antMatchers("/eliminar/**").hasAnyRole("ADMIN")
 		.antMatchers("/factura/**").hasAnyRole("ADMIN")
-		.anyRequest().authenticated();
-	}
+		.anyRequest().authenticated()
+		.and()
+		    .formLogin()
+		    .successHandler(successHandler)
+		    .loginPage("/login")
+		    .permitAll()
+		.and()
+		.logout().permitAll()
+		.and()
+		.exceptionHandling().accessDeniedPage("/error_403");
 
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-
-		return new BCryptPasswordEncoder();
 	}
 
 	@Autowired
-	public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception{
-		PasswordEncoder encoder = passwordEncoder();
-		//Tres diferentes maneras de declarar el siguiente bloque de código haciendo uso de funciones lambda
-		/*UserBuilder users = User.builder().passwordEncoder(password -> {
-			return encoder.encode(password);
-		});*/
-		/*UserBuilder users = User.builder().passwordEncoder(password -> encoder.encode(password));*/
-		UserBuilder users = User.builder().passwordEncoder(encoder :: encode);
-
+	public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception
+	{
+		/*
+		 * Deprecated
+		 * UserBuilder users = User.withDefaultPasswordEncoder();
+		 * */
+		
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+		
 		build.inMemoryAuthentication()
 		.withUser(users.username("admin").password("12345").roles("ADMIN", "USER"))
 		.withUser(users.username("andres").password("12345").roles("USER"));
--- src/main/java/com/ideasbolsa/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/ideasbolsa/springboot/app/controllers/ClienteController.java
@@ -1,20 +1,21 @@
 package com.ideasbolsa.springboot.app.controllers;
 
-
-
-
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.Map;
 
 import javax.validation.Valid;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -33,41 +34,38 @@ import com.ideasbolsa.springboot.app.models.service.IClienteService;
 import com.ideasbolsa.springboot.app.models.service.IUploadFileService;
 import com.ideasbolsa.springboot.app.util.paginator.PageRender;
 
-
-
 @Controller
 @SessionAttributes("cliente")
 public class ClienteController {
 
+	protected final Log logger = LogFactory.getLog(this.getClass());
 	@Autowired
 	private IClienteService clienteService;
-	
-	/*Inyectando el servicio IUploadFileService para poder usar sus métodos */
+
+	/* Inyectando el servicio IUploadFileService para poder usar sus métodos */
 	@Autowired
 	private IUploadFileService uploadFileService;
-	
-	/*Método verFoto*/
+
+	/* Método verFoto */
 	@GetMapping(value = "/uploads/{filename:.+}")
-	public ResponseEntity<Resource> verFoto(@PathVariable String filename){
-		
+	public ResponseEntity<Resource> verFoto(@PathVariable String filename) {
+
 		Resource recurso = null;
-		
+
 		try {
 			recurso = uploadFileService.load(filename);
 		} catch (MalformedURLException e) {
 			e.getStackTrace();
 		}
-		
-		return ResponseEntity.ok()
-				.header(org.springframework.http.HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" +recurso.getFilename()+"\"")
-				.body(recurso);
+
+		return ResponseEntity.ok().header(org.springframework.http.HttpHeaders.CONTENT_DISPOSITION,
+				"attachment; filename=\"" + recurso.getFilename() + "\"").body(recurso);
 	}
-	
-	
-	/*Método ver*/
-	@GetMapping(value="/ver/{id}")
-	public String ver(@PathVariable(value="id") Long id, Map< String, Object > model, RedirectAttributes flash) {
-		Cliente cliente = clienteService.fetchByWithFacturas(id);//clienteService.findOne(id);
+
+	/* Método ver */
+	@GetMapping(value = "/ver/{id}")
+	public String ver(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
+		Cliente cliente = clienteService.fetchByWithFacturas(id);// clienteService.findOne(id);
 		if (cliente == null) {
 			flash.addFlashAttribute("error", "El cliente no existe ne la base de datos");
 			return "redirect:/listar";
@@ -76,25 +74,37 @@ public class ClienteController {
 		model.put("titulo", "Detalle cliente: " + cliente.getNombre());
 		return "ver";
 	}
-	
-	
-	/*Método listar*/
-	@RequestMapping(value = {"/listar", "/"}, method = RequestMethod.GET)
-	public String listar(@RequestParam(name="page", defaultValue="0") int page, Model model) {
-		
+
+	/* Método listar */
+	@RequestMapping(value = { "/listar", "/" }, method = RequestMethod.GET)
+	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model,
+			Authentication authentication) {
+
+		if (authentication != null) {
+			
+			logger.info("Hola usuario autentificado,  tu username es ".concat(authentication.getName()));
+		}
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+		if (auth != null) {
+
+			logger.info("Utilizando forma estática SecurityContextHolder.getContext().getAuthentication(): Usuario identificado,  tu username es: ".concat(authentication.getName()));
+
+		}
+
 		Pageable pageRequest = PageRequest.of(page, 5);
-		
+
 		Page<Cliente> clientes = clienteService.findAll(pageRequest);
-		
+
 		PageRender<Cliente> pageRender = new PageRender<Cliente>("/listar", clientes);
 		model.addAttribute("titulo", "Listado de clientes");
 		model.addAttribute("clientes", clientes);
 		model.addAttribute("page", pageRender);
 		return "listar";
 	}
-	
-	
-	/*Método crear*/
+
+	/* Método crear */
 	@RequestMapping(value = "/form")
 	public String crear(Map<String, Object> model) {
 		Cliente cliente = new Cliente();
@@ -103,16 +113,16 @@ public class ClienteController {
 		return "form";
 	}
 
-	
-	/*Método editar**/
-	//RedirectAttributes: librería que permite mandar un mensaje cuando se realiza una acción de redireccionamiento  */
-	@RequestMapping(value="/form/{id}")
-	public String editar(@PathVariable(value="id") Long id, Map<String, Object> model, RedirectAttributes flash) {
-		Cliente cliente =null;
-		
+	/* Método editar **/
+	// RedirectAttributes: librería que permite mandar un mensaje cuando se realiza
+	// una acción de redireccionamiento */
+	@RequestMapping(value = "/form/{id}")
+	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
+		Cliente cliente = null;
+
 		if (id > 0) {
 			cliente = clienteService.findOne(id);
-			if(cliente == null) {
+			if (cliente == null) {
 				flash.addFlashAttribute("error", "El Id del cliente no existe en la base de datos!");
 				return "redirect:/listar";
 			}
@@ -122,66 +132,73 @@ public class ClienteController {
 		}
 		model.put("cliente", cliente);
 		model.put("titulo", "Editar cliente");
-		return"form";
+		return "form";
 	}
-	
-	
-	/*Método guardar*/
+
+	/* Método guardar */
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
-	public String guardar(@Valid  Cliente cliente, BindingResult result, Model model, 
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model,
 			@RequestParam("file") MultipartFile foto, RedirectAttributes flash, SessionStatus status) {
-		
-		if(result.hasErrors()) {
+
+		if (result.hasErrors()) {
 			model.addAttribute("titulo", "Formulario de cliente");
 			return "form";
 		}
-		/*Condición para saber si existe una foto cargada en el perfil del usuario(cliente)*/
-		if(!foto.isEmpty()) {
-			/*Condición que permite saber si la foto esta en el perfil, si esta en el perfil con Id existente,
-			 * que existe foto cargada en el perfíl, si la foto es mayor a 0.....
-			 * Todas estas condiciones son las que existen para saber si hay una foto cargada en el perfíl*/
-			if (cliente.getId() !=null && cliente.getId() > 0 && cliente.getFoto()!=null && cliente.getFoto().length() > 0) {
-				
+		/*
+		 * Condición para saber si existe una foto cargada en el perfil del
+		 * usuario(cliente)
+		 */
+		if (!foto.isEmpty()) {
+			/*
+			 * Condición que permite saber si la foto esta en el perfil, si esta en el
+			 * perfil con Id existente, que existe foto cargada en el perfíl, si la foto es
+			 * mayor a 0..... Todas estas condiciones son las que existen para saber si hay
+			 * una foto cargada en el perfíl
+			 */
+			if (cliente.getId() != null && cliente.getId() > 0 && cliente.getFoto() != null
+					&& cliente.getFoto().length() > 0) {
+
 				uploadFileService.delete(cliente.getFoto());
 			}
-			
+
 			String uniqueFilename = null;
-			
+
 			try {
-				
-			uniqueFilename = uploadFileService.copy(foto);
+
+				uniqueFilename = uploadFileService.copy(foto);
 
 			} catch (IOException e) {
 
 				e.printStackTrace();
-			}			
-			/*Menasje al conbcluir la acción */
-			flash.addFlashAttribute("info", "Has subido correctamente '" + uniqueFilename + "'");
-			/*pasar el nombre de la foto al clinete para que quede almacenada en la base de datos y quede 
-			 * a disposición para almacenar al cliente*/
-			cliente.setFoto(uniqueFilename);
 			}
+			/* Menasje al conbcluir la acción */
+			flash.addFlashAttribute("info", "Has subido correctamente '" + uniqueFilename + "'");
+			/*
+			 * pasar el nombre de la foto al clinete para que quede almacenada en la base de
+			 * datos y quede a disposición para almacenar al cliente
+			 */
+			cliente.setFoto(uniqueFilename);
+		}
 
-		String mensajeFlash = (cliente.getId() != null)? "Cliente editado con éxito": "Cliente creado con éxito";
+		String mensajeFlash = (cliente.getId() != null) ? "Cliente editado con éxito" : "Cliente creado con éxito";
 		clienteService.save(cliente);
 		status.setComplete();
 		flash.addFlashAttribute("success", mensajeFlash);
 		return "redirect:listar";
 	}
-	
 
-	/*Método eliminar*/
-	@RequestMapping(value="/eliminar/{id}")
-	public String eliminar(@PathVariable(value="id") Long id, RedirectAttributes flash ) {
-		if(id > 0) {
-			
+	/* Método eliminar */
+	@RequestMapping(value = "/eliminar/{id}")
+	public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
+		if (id > 0) {
+
 			Cliente cliente = clienteService.findOne(id);
 			clienteService.delete(id);
 			flash.addFlashAttribute("warning", "Cliente eliminado con éxito");
-			
-				if (uploadFileService.delete(cliente.getFoto())) {
-					flash.addFlashAttribute("info","Foto: " + cliente.getFoto() + " eliminada con éxito!");
-				}
+
+			if (uploadFileService.delete(cliente.getFoto())) {
+				flash.addFlashAttribute("info", "Foto: " + cliente.getFoto() + " eliminada con éxito!");
+			}
 		}
 		return "redirect:/listar";
 	}
