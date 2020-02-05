de3193c2c3725344610f63d0997355763d51674f
CesarVE <thecesarvargas@gmail.com>
2020-02-04 23:45:21 -0600

    Obteniendo programaticamente el role del usuario en ClienteController

--- src/main/java/com/ideasbolsa/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/ideasbolsa/springboot/app/controllers/ClienteController.java
@@ -2,20 +2,30 @@ package com.ideasbolsa.springboot.app.controllers;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Collection;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -75,25 +85,46 @@ public class ClienteController {
 		return "ver";
 	}
 
-	/* Método listar */
 	@RequestMapping(value = { "/listar", "/" }, method = RequestMethod.GET)
 	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model,
-			Authentication authentication) {
+			Authentication authentication, HttpServletRequest request) {
 
 		if (authentication != null) {
-			
-			logger.info("Hola usuario autentificado,  tu username es ".concat(authentication.getName()));
+			logger.info("Hola usuario autenticado, tu username es: ".concat(authentication.getName()));
 		}
 
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
 		if (auth != null) {
-
-			logger.info("Utilizando forma estática SecurityContextHolder.getContext().getAuthentication(): Usuario identificado,  tu username es: ".concat(authentication.getName()));
-
+			logger.info(
+					"Utilizando forma estática SecurityContextHolder.getContext().getAuthentication(): Usuario autenticado: "
+							.concat(auth.getName()));
 		}
 
-		Pageable pageRequest = PageRequest.of(page, 5);
+		if (hasRole("ROLE_ADMIN")) {
+			logger.info("Hola ".concat(auth.getName()).concat(" tienes acceso!"));
+		} else {
+			logger.info("Hola ".concat(auth.getName()).concat(" NO tienes acceso!"));
+		}
+
+		SecurityContextHolderAwareRequestWrapper securityContext = new SecurityContextHolderAwareRequestWrapper(request,
+				"");
+
+		if (securityContext.isUserInRole("ROLE_ADMIN")) {
+			logger.info("Forma usando SecurityContextHolderAwareRequestWrapper: Hola ".concat(auth.getName())
+					.concat(" tienes acceso!"));
+		} else {
+			logger.info("Forma usando SecurityContextHolderAwareRequestWrapper: Hola ".concat(auth.getName())
+					.concat(" NO tienes acceso!"));
+		}
+
+		if (request.isUserInRole("ROLE_ADMIN")) {
+			logger.info("Forma usando HttpServletRequest: Hola ".concat(auth.getName()).concat(" tienes acceso!"));
+		} else {
+			logger.info("Forma usando HttpServletRequest: Hola ".concat(auth.getName()).concat(" NO tienes acceso!"));
+		}
+
+		Pageable pageRequest = PageRequest.of(page, 4);
 
 		Page<Cliente> clientes = clienteService.findAll(pageRequest);
 
@@ -201,5 +232,34 @@ public class ClienteController {
 			}
 		}
 		return "redirect:/listar";
+	}
+
+	private boolean hasRole(String role) {
+
+		SecurityContext context = SecurityContextHolder.getContext();
+
+		if (context == null) {
+			return false;
+		}
+
+		Authentication auth = context.getAuthentication();
+
+		if (auth == null) {
+			return false;
+		}
+
+		Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
+
+		return authorities.contains(new SimpleGrantedAuthority(role));
+
+		/*
+		 * for(GrantedAuthority authority: authorities) {
+		 * if(role.equals(authority.getAuthority())) {
+		 * logger.info("Hola usuario ".concat(auth.getName()).concat(" tu role es: "
+		 * .concat(authority.getAuthority()))); return true; } }
+		 * 
+		 * return false;
+		 */
+
 	}
 }
