d7617471791dbe3a7b2086ab572c7ed97680c578
CesarVE <thecesarvargas@gmail.com>
2020-02-11 16:33:54 -0600

    SPRING SECURITY: Autenticación con JPA. Creando las entidades necesarias Usuario y Role

--- src/main/java/com/ideasbolsa/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/ideasbolsa/springboot/app/controllers/ClienteController.java
@@ -16,7 +16,6 @@ import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
